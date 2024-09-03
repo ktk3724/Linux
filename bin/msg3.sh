d968a7ed6c3acd0527428a11f9f79b26f674dfd5
ktk3724 <ktk3724@naver.com>
2024-09-03 18:48:29 +0900

    2nd commit

---
+++ bin/msg3.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+IP1=192.168.10.10
+PORT1=1514
+
+while true
+do
+
+facility_num="$(expr $RANDOM % 3)"
+case $facility_num in
+	0) facility="user" ;; 
+	1) facility="local0" ;;
+	2) facility="kern" ;;
+	*) exit 1 ;;
+esac
+
+level_num="$(expr $RANDOM % 3)"
+# level_num="$(expr $RANDOM % 6)"
+case $level_num in 
+	0) level="notice" ;;
+	1) level="warn" ;;
+	2) level="crit" ;;
+#   	3) level="debug" ;;
+#   	4) level="error" ;;
+#   	5) level="alert" ;;
+	*) exit 2 ;;
+esac
+
+msg_num="$(expr $RANDOM % 3)"
+case $msg_num in 
+	0) msg="hello graylog server($RANDOM) from 192.168.10.10" ;;
+	1) msg="test graylog server($RANDOM) from 192.168.10.10" ;;
+	2) msg="**^^** graylog server($RAMDOM) from 192.168.10.10" ;;
+	*) exit 3 ;;
+esac
+
+logger -n $IP1 -P $PORT1 -d -p "$facility.$level" "$msg"
+sleep 10       
+# sleep $(expr $RANDOM % 10)
+
+done
